73486f29e2bb3438edad9eae73fcec36cfd16c88
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-07-29 03:28:10 +0600

    productcard component added

---
+++ components/ProductCard.tsx
@@ -0,0 +1,85 @@
+import { Product } from '@/sanity.types'
+import { urlFor } from '@/sanity/lib/image'
+import { Flame, StarIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import AddToWishlistButton from './AddToWishlistButton'
+import { Title } from './ui/text'
+
+const ProductCard = ({ product }: { product: Product }) => {
+  return (
+    <div className='border-dark_blue/20 group rounded-md border bg-white text-sm'>
+      <div className='group bg-shop_light_bg relative overflow-hidden'>
+        {product?.images && (
+          <Image
+            src={urlFor(product?.images[0]).url()}
+            alt='Product Image'
+            loading='lazy'
+            width={700}
+            height={700}
+            className={`bg-shop_light_bg hoverEffect h-64 w-full overflow-hidden object-contain transition-transform ${product?.stock !== 0 ? 'group-hover:scale-105' : 'opacity-50'}`}
+          />
+        )}
+        <AddToWishlistButton product={product} />
+        {product?.status === 'sale' && (
+          <p className='border-darkColor/50 group-hover:border-shop_light_green hoverEffect group-hover:text-shop_light_green absolute top-2 left-2 z-10 rounded-full border px-2 text-xs'>
+            Sale!
+          </p>
+        )}
+        {product?.status === 'new' && (
+          <p className='border-darkColor/50 group-hover:border-shop_light_green hoverEffect group-hover:text-shop_light_green absolute top-2 left-2 z-10 rounded-full border px-2 text-xs'>
+            New!
+          </p>
+        )}
+        {product?.status === 'hot' && (
+          <Link
+            href={'/deal'}
+            className='border-shop_orange/50 group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect absolute top-2 left-2 z-10 rounded-full border p-1'
+          >
+            <Flame
+              size={18}
+              fill='#fb6c08'
+              className='text-shop_orange/50 group-hover:text-shop_orange hoverEffect'
+            />
+          </Link>
+        )}
+      </div>
+      <div className='p-3'>
+        {product?.categories && (
+          <p className='text-shop_light_text line-clamp-1 text-xs uppercase'>
+            {product?.categories?.map(cat => cat).join(', ')}
+          </p>
+        )}
+        <Title className='line-clamp-1 text-sm'>{product?.name}</Title>
+        <div className='flex items-center gap-0.5'>
+          <div className='flex items-center'>
+            {[...Array(5)].map((_, index) => (
+              <StarIcon
+                size={12}
+                key={index}
+                className={
+                  index < 4
+                    ? 'text-shop_lighter_green'
+                    : 'text-shop_lighter_text'
+                }
+                fill={index < 4 ? '#93d991' : '#ababab'}
+              />
+            ))}
+          </div>
+          <p className='text-shop_light_text text-xs tracking-wide'>
+            5 Reviews
+          </p>
+        </div>
+        <div className='flex items-center gap-2.5'>
+          <p className='font-medium'>In Stock</p>
+          <p
+            className={`${product?.stock === 0 ? 'text-red-600' : 'text-shop_light_green/80 font-semibold'}`}
+          >
+            {(product?.stock as number) > 0 ? product?.stock : 'unavailable'}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ProductCard
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -4,9 +4,14 @@ import { useEffect, useState } from 'react'
 import HomeTabbar from './HomeTabbar'
 import { productType } from '@/constants'
 import { client } from '@/sanity/lib/client'
+import { AnimatePresence, motion } from 'motion/react'
+import { Loader2 } from 'lucide-react'
+import NoProductAvailable from './NoProductAvailable'
+import ProductCard from './ProductCard'
+import { Product } from '@/sanity.types'
 
 const ProductGrid = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState<Product[]>([])
   const [loading, setLoading] = useState(false)
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || '')
 
@@ -21,7 +26,7 @@ const ProductGrid = () => {
       setLoading(true)
       try {
         const response = await client.fetch(query, params)
-        console.log(response)
+        setProducts(response)
       } catch (error) {
         console.error('products fetching errr:', error)
       } finally {
@@ -35,7 +40,31 @@ const ProductGrid = () => {
   return (
     <div>
       <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
-      {loading ? <div></div> : <>products</>}
+      {loading ? (
+        <div className='mt-10 flex min-h-80 w-full flex-col items-center justify-center gap-4 bg-gray-100 py-10'>
+          <div className='flex items-center space-x-2 text-blue-600'>
+            <Loader2 className='h-5 w-6 animate-spin' />
+            <span>Product is loading...</span>
+          </div>
+        </div>
+      ) : products?.length ? (
+        <div className='mt-10 grid grid-cols-2 gap-2.5 md:grid-cols-3 lg:grid-cols-5'>
+          {products?.map(product => (
+            <AnimatePresence key={product?._id}>
+              <motion.div
+                layout
+                initial={{ opacity: 0.2 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <ProductCard product={product} />
+              </motion.div>
+            </AnimatePresence>
+          ))}
+        </div>
+      ) : (
+        <NoProductAvailable selectedTab={selectedTab} />
+      )}
     </div>
   )
 }
